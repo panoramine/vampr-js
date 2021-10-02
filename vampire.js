64a44ba6202ba7823962dacc5e871ae1d3732dac
panoramine <camila.c.a.salles@gmail.com>
2021-10-01 19:49:11 -0700

    vampr

--- vampire.js
+++ vampire.js
@@ -10,22 +10,37 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
 
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+  // Returns true if this vampire is more senior than the other vampire. (Who is closer
+  // to the original vampire)
   isMoreSeniorThan(vampire) {
 
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -36,9 +51,90 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (!this.creator) {
+      return this;
+    }
 
+    if (!vampire.creator) {
+      return vampire;
+    }
+
+    if (this === vampire) {
+      return this;
+    }
+
+    if (this.numberOfVampiresFromOriginal === 1 || vampire.numberOfVampiresFromOriginal === 1) {
+      return this.creator;
+    }
+
+    if (this.isMoreSeniorThan(vampire)) {
+      let seniorV = this.numberOfVampiresFromOriginal;
+      let youngerV = vampire.numberOfVampiresFromOriginal;
+      let counter = youngerV;
+      let linkFromThePast = this.creator;
+      let youngerAntecedent = vampire.creator;
+      
+      while (seniorV) {
+        
+        while (youngerV) {
+
+          if (youngerAntecedent === this) {
+            
+            return this;
+          }
+          
+          if (linkFromThePast === youngerAntecedent) {
+            
+            return linkFromThePast;
+          }
+
+          if (youngerAntecedent.creator) {
+            youngerAntecedent = youngerAntecedent.creator;
+          }
+          youngerV--;
+        }
+
+        seniorV--;
+        linkFromThePast = linkFromThePast.creator;
+        counter--;
+        youngerV = counter;
+      }
+    }
+
+    if (!this.isMoreSeniorThan(vampire)) {
+      let seniorV = vampire.numberOfVampiresFromOriginal;
+      let youngerV = this.numberOfVampiresFromOriginal;
+      let counter = youngerV;
+      let linkFromThePast = vampire.creator;
+      let youngerAntecedent = this.creator;
+ 
+      while (seniorV) {
+        
+        while (youngerV) {
+
+          if (youngerAntecedent === vampire) {
+            
+            return vampire;
+          }
+          
+          if (linkFromThePast === youngerAntecedent) {
+            
+            return linkFromThePast;
+          }
+
+          if (youngerAntecedent.creator) {
+            youngerAntecedent = youngerAntecedent.creator;
+          }
+          youngerV--;
+        }
+
+        seniorV--;
+        linkFromThePast = linkFromThePast.creator;
+        counter--;
+        youngerV = counter;
+      }
+    }
   }
 }
 
 module.exports = Vampire;
-
